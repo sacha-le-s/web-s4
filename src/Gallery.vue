66e431a89339cb3593c38eb5939810d3369f14ab
Sacha <chouvinsacha@gmail.com>
2022-03-08 17:26:09 +0100

    first vue files

---
+++ src/Gallery.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="champion-gallery">
+		<ChampionCard/>
+  </div>
+</template>
+
+<script>
+import ChampionCard from './components/ChampionCard.vue'
+
+export default {
+  name: 'ChampionGallery',
+	components: {
+		ChampionCard
+	}
+}
+</script>
+
+<style></style>
